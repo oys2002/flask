eeabab5de376a0748c8ab75655df65464578efc3
oys2002 <yaminisaraswathi.oggu@gmail.com>
2023-09-21 10:26:10 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,13 +1,13 @@
 from flask import Flask, request, render_template, redirect, url_for
-
+import sqlite3
 
 app = Flask(__name__)
 
-
-users = {
-    'user1': {'password': 'password1', 'email': 'user1@example.com'},
-    'user2': {'password': 'password2', 'email': 'user2@example.com'}
-}
+# Function to create a database connection
+def get_db_connection():
+    conn = sqlite3.connect('users.db')
+    conn.row_factory = sqlite3.Row
+    return conn
 
 @app.route('/')
 def login():
@@ -17,16 +17,19 @@ def login():
 def login_post():
     username = request.form.get('username')
     password = request.form.get('password')
-    email = request.form.get('email')
 
-    if username in users and users[username]['password'] == password:
-        email = users[username]['email']
-        return f'Logged in as {username} (Email: {email})'
+    conn = get_db_connection()
+    cursor = conn.cursor()
+    cursor.execute('SELECT * FROM users WHERE username = ?', (username,))
+    user = cursor.fetchone()
+    conn.close()
+
+    if user and user['password'] == password:
+        return f'Logged in as {username} (Email: {user["email"]})'
     else:
         return 'Invalid credentials. Please try again.'
 
-
-@app.route("/api", methods = ['GET'])
+@app.route("/api", methods=['GET'])
 def api1():
     return render_template("show.html", ins="Checking Sample API")
 
